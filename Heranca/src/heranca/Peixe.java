c791e2525cf0d489a8706f657f229f36aab93949
Adifexx <arthuradnet@hotmail.com>
2026-04-30 10:16:03 -0300

    Add files via upload

---
+++ Heranca/src/heranca/Peixe.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package heranca;
+
+/**
+ *
+ * @author alunolab10
+ */
+public class Peixe extends Animal {
+    
+    void respirar(){System.out.println("Peixe: respirar() - AGUA");}
+    
+}
